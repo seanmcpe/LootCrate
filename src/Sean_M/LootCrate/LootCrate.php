becf81c0c0adc68eb029e6082cbb2e3fdea0190b
Sean <sean@hydreon.com>
2016-01-10 16:14:42 -0500

    Update LootCrate.php

--- src/Sean_M/LootCrate/LootCrate.php
+++ src/Sean_M/LootCrate/LootCrate.php
@@ -2,11 +2,13 @@
 
 namespace Sean_M\LootCrate;
 
-use pocketmine\Server;
-use pocketmine\scheduler\PluginTask;
 use pocketmine\utils\TextFormat;
 use pocketmine\Player;
 
-class LootCrate extends PluginTask {
+/*
+    public function __construct(Main $plugin 
 
-//    public function __construct(Main $plugin 
+    public function onRun($currentTick) {
+
+    }
+*/
