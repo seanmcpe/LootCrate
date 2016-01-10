7f1526174a5b6716aeca87f7481fc9cfb3f6430b
Sean <sean@hydreon.com>
2016-01-10 14:30:15 -0500

    Update Main.php

--- src/Sean_M/LootCrate/Main.php
+++ src/Sean_M/LootCrate/Main.php
@@ -14,12 +14,12 @@ class Main extends PluginBase implements Listener {
      public function onEnable() {
         @mkdir($this->getDataFolder()); 
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->saveDefaultConfig();
         $this->config = (new Config($this->getDataFolder(). "config.yml", Config::YAML, array(
         "time" => 10)));
         $this->getLogger()->info(TextFormat::GREEN . "LootCrate by Sean_M enabled!");
            $time = $this->config["time"];
-           $this->getServer()->getScheduler()->scheduleRepeatingTask(new LootCrate($this), $time * 20);  
+           $this->getServer()->getScheduler()->scheduleRepeatingTask(new LootCrate($this), $time * 20);
+           $this->saveDefaultConfig();
      }
 
      public function onDisable() {
