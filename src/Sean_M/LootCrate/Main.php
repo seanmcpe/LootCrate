c4a58b214fff3fb081c8367b99cfbb83fe57c34e
Sean <sean@hydreon.com>
2016-01-10 13:54:12 -0500

    Update Main.php

--- src/Sean_M/LootCrate/Main.php
+++ src/Sean_M/LootCrate/Main.php
@@ -1,1 +1,25 @@
+<?php
 
+namespace Sean_M\LootCrate;
+
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat;
+use pocketmine\Player;
+
+class Main extends PluginBase implements Listener {
+    public $config;
+
+     public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->saveDefaultConfig();
+        $this->config = $this->getConfig()->getAll();
+        $this->getLogger()->info(TextFormat::GREEN . "LootCrate by Sean_M enabled!");
+     }
+
+     public function onDisable() {
+        $this->getLogger()->info(TextFormat::RED . "LootCrate by Sean_M disabled!");
+     }
+// soon
+}  
