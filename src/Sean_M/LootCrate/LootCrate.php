bacd527d883a53ae31bc264054f179290c180330
Sean <sean@hydreon.com>
2016-01-10 14:16:56 -0500

    Update LootCrate.php

--- src/Sean_M/LootCrate/LootCrate.php
+++ src/Sean_M/LootCrate/LootCrate.php
@@ -1,1 +1,12 @@
+<?php
 
+namespace Sean_M\LootCrate;
+
+use pocketmine\Server;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\utils\TextFormat;
+use pocketmine\Player;
+
+class LootCrate extends PluginTask {
+
+//    public function __construct(Main $plugin 
