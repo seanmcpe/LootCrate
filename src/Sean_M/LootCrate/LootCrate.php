abd5bb542bc0b19ffffcc09d4c1136c95c6d9842
Sean <sean@hydreon.com>
2016-01-11 07:57:21 -0500

    Update LootCrate.php

--- src/Sean_M/LootCrate/LootCrate.php
+++ src/Sean_M/LootCrate/LootCrate.php
@@ -4,11 +4,21 @@ namespace Sean_M\LootCrate;
 
 use pocketmine\utils\TextFormat;
 use pocketmine\Player;
+use pocketmine\level\Level;
+use pocketmine\math\Vector3;
 
-/*
-    public function __construct(Main $plugin 
-
-    public function onRun($currentTick) {
+    public function __construct(Main $plugin) {
 
     }
+
+    public function onRun($currentTick) {
+       $level = $this->getLevel();
+       $player = $this->player;
+       $x = $this->config["x"];
+       $y = $this->config["y"];
+       $z = $this->config["z"];
+       $pos = new Vector3($x, $y, $z);
+/*       $chest = Block::fromString(Chest);
+          $level->setBlock($pos, $chest);
 */
+    }
