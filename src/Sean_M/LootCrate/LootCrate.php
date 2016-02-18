163d8d97cf542b83335b0beae03e32349b066059
Sean <sean@hydreon.com>
2016-02-18 08:24:31 -0500

    (Add) broadcastMessage

--- src/Sean_M/LootCrate/LootCrate.php
+++ src/Sean_M/LootCrate/LootCrate.php
@@ -2,11 +2,13 @@
 
 namespace Sean_M\LootCrate;
 
+// organize soon
 use pocketmine\utils\TextFormat;
 use pocketmine\inventory\Inventory;
 use pocketmine\block\Block;
 use pocketmine\level\Level;
 use pocketmine\math\Vector3;
+use pocketmine\Server;
 
     public function __construct(Main $plugin) {
 
@@ -23,6 +25,7 @@ use pocketmine\math\Vector3;
        $contents = array_rand($items);
           $level->setBlock($pos, $chest);
           $chest->setContents($items[$contents]);
+//        $this->broadcastMessage("A crate has been spawned at" . $x $y $z . "!");
              if($chest->firstEmpty() == -1) {
                 $air = Block::fromString("Air");
                 $level->setBlock($pos, $air);
