8920377e705580f1fce7d5b034c660770f620769
Sean <sean@hydreon.com>
2016-02-20 12:29:01 -0500

    Update LootCrate.php

--- src/Sean_M/LootCrate/LootCrate.php
+++ src/Sean_M/LootCrate/LootCrate.php
@@ -26,7 +26,7 @@ class LootCrate extends PluginTask {
          $contents = array_rand($items);
             $level->setBlock($pos, $chest);
             $chest->setContents($items[$contents]);
-//          $this->broadcastMessage(TF::GREEN "A crate has been spawned at" . $x $y $z . "!");
+            $this->getServer()->broadcastMessage(TF::GREEN "A crate has been spawned at" . $x $y $z . "!");
                if($chest->firstEmpty() == -1) {
                   $air = Block::fromString("Air");
                   $level->setBlock($pos, $air);
