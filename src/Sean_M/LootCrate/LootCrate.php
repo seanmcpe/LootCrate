1a2adb203d7f601c7c6c2d2f79f327bedc5712fe
Sean <sean@hydreon.com>
2016-02-20 12:26:44 -0500

    OOPS!

--- src/Sean_M/LootCrate/LootCrate.php
+++ src/Sean_M/LootCrate/LootCrate.php
@@ -9,24 +9,27 @@ use pocketmine\utils\TextFormat as TF;
 use pocketmine\Server;
 use pocketmine\math\Vector3;
 
-    public function __construct(Main $plugin) {
+class LootCrate extends PluginTask {
 
-    }
+      public function __construct(Main $plugin) {
 
-    public function onRun($currentTick) {
-       $level = $this->getLevel();
-       $x = $this->config["x"];
-       $y = $this->config["y"];
-       $z = $this->config["z"];
-       $pos = new Vector3($x, $y, $z);
-       $chest = Block::fromString("Chest");
-       $items = array($this->config["contents"]);
-       $contents = array_rand($items);
-          $level->setBlock($pos, $chest);
-          $chest->setContents($items[$contents]);
-//        $this->broadcastMessage(TF::GREEN "A crate has been spawned at" . $x $y $z . "!");
-             if($chest->firstEmpty() == -1) {
-                $air = Block::fromString("Air");
-                $level->setBlock($pos, $air);
+      }
+
+      public function onRun($currentTick) {
+         $level = $this->getLevel();
+         $x = $this->config["x"];
+         $y = $this->config["y"];
+         $z = $this->config["z"];
+         $pos = new Vector3($x, $y, $z);
+         $chest = Block::fromString("Chest");
+         $items = array($this->config["contents"]);
+         $contents = array_rand($items);
+            $level->setBlock($pos, $chest);
+            $chest->setContents($items[$contents]);
+//          $this->broadcastMessage(TF::GREEN "A crate has been spawned at" . $x $y $z . "!");
+               if($chest->firstEmpty() == -1) {
+                  $air = Block::fromString("Air");
+                  $level->setBlock($pos, $air);
              }
-    }
+      }
+}
