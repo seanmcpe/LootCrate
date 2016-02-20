2371f441e527c19f41ae1437b830a1d5be1cb9f9
Sean <sean@hydreon.com>
2016-02-20 14:23:04 -0500

    Thanks KnownUnown

--- src/Sean_M/LootCrate/LootCrate.php
+++ src/Sean_M/LootCrate/LootCrate.php
@@ -5,28 +5,33 @@ namespace Sean_M\LootCrate;
 use pocketmine\block\Block;
 use pocketmine\inventory\Inventory;
 use pocketmine\level\Level;
+use pocketmine\scheduler\PluginTask;
 use pocketmine\utils\TextFormat as TF;
 use pocketmine\Server;
 use pocketmine\math\Vector3;
 
 class LootCrate extends PluginTask {
 
-      public function __construct(Main $plugin) {
+   public $plugin;
 
+      public function __construct(Main $plugin) {
+         parent::__construct($plugin);
+         $this->plugin = $plugin;
       }
 
       public function onRun($currentTick) {
-         $level = $this->getLevel();
-         $x = $this->config["x"];
-         $y = $this->config["y"];
-         $z = $this->config["z"];
+         $server = Server::getInstance();
+         $level = $server->getDefaultLevel();
+         $x = $this->plugin->config["x"];
+         $y = $this->plugin->config["y"];
+         $z = $this->plugin->config["z"];
          $pos = new Vector3($x, $y, $z);
          $chest = Block::fromString("Chest");
-         $items = array($this->config["contents"]);
+         $items = array($this->plugin->config["contents"]);
          $contents = array_rand($items);
             $level->setBlock($pos, $chest);
             $chest->setContents($items[$contents]);
-            $this->getServer()->broadcastMessage(TF::GREEN "A crate has been spawned at" . $x $y $z . "!");
+            $server->broadcastMessage("CRATE SPAWNED");
                if($chest->firstEmpty() == -1) {
                   $air = Block::fromString("Air");
                   $level->setBlock($pos, $air);
